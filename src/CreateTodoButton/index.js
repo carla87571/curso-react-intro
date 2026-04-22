8777d15401651e075be4c17eafc1adb6fd250e69
Carla Mendía <carla87571@hotmail.com>
2026-04-22 18:10:22 -0500

    Refactor: organizar componentes en carpetas, agregar Modal y TodoForm

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import './CreateTodoButton.css';
+
+function CreateTodoButton() {
+  const { setOpenModal } = React.useContext(TodoContext);
+
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={() => setOpenModal(true)}
+    >+</button>
+  );
+}
+
+export { CreateTodoButton };
--- src/TodoSearch.js
+++
@@ -1,9 +0,0 @@
-import './TodoSearch.css';
-
-function TodoSearch() {
-  return (
-    <input className="TodoSearch" placeholder="Leer caballo de troya" />
-  );
-}
-
-export { TodoSearch };
